b3e0cc84a7e55ab3455edca35530a8c5249df212
Elijah <jabor.rs@gmail.com>
2017-09-02 19:52:45 -0400

    Initial commit (no google jsons)

---
+++ app/src/main/java/com/example/willr/testchat/MainActivity.java
@@ -0,0 +1,76 @@
+package com.example.willr.testchat;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.braunster.androidchatsdk.firebaseplugin.firebase.BChatcatNetworkAdapter;
+import com.braunster.chatsdk.Utils.helper.ChatSDKUiHelper;
+import com.braunster.chatsdk.network.BDefines;
+import com.braunster.chatsdk.network.BNetworkManager;
+import com.braunster.chatsdk.activities.ChatSDKLoginActivity;
+import android.content.Intent;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private void initChat() {
+        // This is used for the app custom toast and activity transition
+        ChatSDKUiHelper.initDefault();
+        // Init the network manager
+        BNetworkManager.init(getApplicationContext());
+        // Create a new adapter
+        BChatcatNetworkAdapter adapter = new BChatcatNetworkAdapter(getApplicationContext());
+        // Set the adapter
+        BNetworkManager.sharedManager().setNetworkAdapter(adapter);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        initChat();
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+               // Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+               //         .setAction("Action", null).show();
+                System.out.println("Hello");
+                Intent myIntent = new Intent(MainActivity.this, ChatSDKLoginActivity.class);
+                MainActivity.this.startActivity(myIntent);
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
